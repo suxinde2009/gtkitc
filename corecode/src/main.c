f3d67ee2680165e7dd266f1b0302fca1a4c3b81f
Dean Butcher <butchdeanca@gmail.com>
2015-10-12 21:21:13 -0300

    Added getopt().

--- corecode/src/main.c
+++ corecode/src/main.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sll_tests.h>
 #include <dll_tests.h>
 #include <btree_tests.h>
 
-int main()
+int main(int argc, char** argv)
 {
-	/*sll_test_run();*/
-	dll_test_run();
-	/*btree_test_run();*/
+	int options;
+
+	while((options = getopt(argc, argv, "sdb")) != -1)
+	{
+		switch (options)
+		{
+			case 's':
+				sll_test_run();
+				break;
+			case 'd':
+				dll_test_run();
+				break;
+			case 'b':
+				btree_test_run();
+				break;
+			case '?':
+				puts("Use options s, d or b.");
+				break;
+		}
+	}
 
 	return 0;
 }
